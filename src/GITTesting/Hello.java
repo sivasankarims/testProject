b2352e01be7aeb8fb4b7cff0e4de8fdc6dd72a1f
Sivasankari M S <jesan.sivsan@gmail.com>
2017-11-05 11:37:26 +0100

    Commit check

---
+++ src/GITTesting/Hello.java
@@ -0,0 +1,19 @@
+package GITTesting;
+
+public class Hello {
+
+	public  void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+
+
+	}
+	public void name() {
+		
+		System.out.println("Commit Test in GIT");
+		
+	}
+	
+
+}
---
+++ src/GITTesting/Test2.java
@@ -0,0 +1,19 @@
+package GITTesting;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		Hello obj1 = new Hello();
+		
+		obj1.name();
+		secMethod();
+		
+		
+
+	}
+	public static void secMethod() {
+		System.out.println("Hi");
+		
+	}
+
+}
